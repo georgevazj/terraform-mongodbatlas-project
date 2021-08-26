319ccf3e403c018bc682362b70feb2a6271e2678
Jorge <georgevazj@gmail.com>
2021-08-26 09:56:42 +0200

    Removed provider variables

--- variables.tf
+++ variables.tf
@@ -1,14 +1,3 @@
-# Provider configuration
-variable "mongodbatlas_public_key" {
-  type = string
-  description = "(Required) The public key for the mongodbatlas user"
-}
-
-variable "mongodbatlas_private_key" {
-  type = string
-  description = "(Required) The private key for the mongodbatlas user"
-}
-
 # Mongo Organization
 variable "organization_id" {
   type = string
